fe23d1e747262e0303a06d0a4bf4eea8264c4bed
pedrocb <pedrocb_c-s@hotmail.com>
2016-04-11 23:45:50 +0100

    added interaction with workspaces on lemonbar

--- .bin/lemonbar_workspaces.py
+++ .bin/lemonbar_workspaces.py
@@ -8,19 +8,20 @@ input_converted = json.loads(input_str)
 
 current_workspace_position = 0
 output = ""
-line=" "*15
+line="."*15
 
 for i in range(1,NUMBER_OF_WORKSPACES+1):
     if(input_converted[current_workspace_position]['num']==i):
         if(input_converted[current_workspace_position]['focused']==True):
-            output+= "%{Ucolor1}%{+u}"+line 
+            output+= "%{Ucolor1}%{+u}%{A:"+str(i)+":}"+line
         else: 
-            output+= "%{Ucolor2}%{+u}"+line 
+            output+= "%{Ucolor2}%{+u}%{A:"+str(i)+":}"+line
         if(current_workspace_position < len(input_converted)-1):
             current_workspace_position+=1
     else:
-        output+= "%{Ucolor3}%{+u}"+line 
+        output+= "%{Ucolor3}%{+u}%{A:"+str(i)+":}"+line
     if(i!=NUMBER_OF_WORKSPACES):
-        output+="%{-u}   "
+        output+="%{-u}%{A}   "
 
+output+="%{A}"
 print output
