5c4ede7600608991241b7f715981e6e8b97980a0
pedrocb <pedrocb_c-s@hotmail.com>
2016-04-19 14:18:45 +0100

    made script to toggle transparency of unfocused windows

---
+++ .bin/window_opacity.sh
@@ -0,0 +1,13 @@
+UNFOCUSED_OPACITY=0.7
+killall compton
+STATE="$(cat dotfiles/compton.conf | grep inactive | grep -o -e '[0-9]\(.[0-9]\)\?')"
+CONDITION="$(echo ${STATE}==${UNFOCUSED_OPACITY}|bc)"
+
+if [[ $CONDITION -eq 1 ]]
+then
+    NEW=1
+else
+    NEW=$UNFOCUSED_OPACITY
+fi
+sed -i "/inactive-opacity/ c inactive-opacity = ${NEW};" ~/dotfiles/compton.conf
+compton -b
