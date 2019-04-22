130b795ed940736aca56e1b7888d3a9d1d26ffef
Pedro Belém <pedrocb_c-s@hotmail.com>
2019-04-22 19:44:38 +0100

    Remove i3 from install sh

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
-#i3
-rm -r ~/.config/i3
-ln -s ~/dotfiles/i3 ~/.config/i3
-
 #.bin
 rm -r ~/.bin
 ln -s ~/dotfiles/.bin ~/.bin
@@ -11,9 +7,5 @@ rm ~/.config/compton.conf
 ln -s ~/dotfiles/compton.conf ~/.config/compton.conf
 
 #emacs
-rm ~/.config/.emacs.d
+rm -r ~/.emacs.d
 ln -s ~/dotfiles/.emacs.d ~/.emacs.d
-
-#bthemer
-rm ~/.bthemer
-ln -s ~/dotfiles/.bthemer ~/.bthemer
